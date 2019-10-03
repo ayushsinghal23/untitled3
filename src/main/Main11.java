f6d3f2c9a5adf2ae7574f4948b453a3831b5e3ce
ayushsinghal23 <ayush.singhal_cs18@gla.ac.in>
2019-10-04 01:11:09 +0530

    some changes in the logic of question 10

--- src/main/Main11.java
+++ src/main/Main11.java
@@ -2,21 +2,21 @@ package main;
 
 import java.util.Scanner;
 
-public class Main10 {
+public class Main11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine().trim();
         char word = scanner.next().charAt(0);
         scanner.close();
-            for (int i = 0; i < input.length(); i++) {
-                if (input.charAt(i) == word) {
-                    System.out.println(i + 1);
-                    break;
-                }
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == word) {
+                System.out.println(i + 1);
 
             }
+
         }
-
-
     }
+
+
+}
 
