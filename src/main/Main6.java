e104e7e76bf94b2c8afca8539ab5c085d8c641bb
ayushsinghal23 <ayush.singhal_cs18@gla.ac.in>
2019-10-03 22:49:11 +0530

    question 6 completed

---
+++ src/main/Main6.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        input = input.toUpperCase();
+        System.out.println(input);
+    }
+}
